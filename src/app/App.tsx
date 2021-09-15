5488faba79f87bd4055aa760925bc77791aa2151
Przemysław Mazur <przemek1713@gmail.com>
2021-09-15 22:19:07 +0200

    base eslint and ts template config

---
+++ src/app/App.tsx
@@ -0,0 +1,10 @@
+import { createTheme } from "@material-ui/core";
+import React from "react";
+
+const theme = createTheme( {
+  spacing: 4,
+} );
+
+export const App = (): React.ReactElement => (
+  <div />
+);
